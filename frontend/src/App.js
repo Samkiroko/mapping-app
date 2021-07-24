10a432878ff8d4efe37de384e634de32808a9750
Samkiroko <kirokodev@gmail.com>
2021-07-24 11:46:30 +0300

    adding new pins

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,8 +6,10 @@ import { format } from 'timeago.js'
 import './app.css'
 
 function App() {
+  const currentUsername = 'john'
   const [pins, setPins] = useState([])
   const [currentPlaceId, setCurrentPlaceId] = useState(null)
+  const [newPlace, setNewPlace] = useState(null)
   const [viewport, setViewport] = useState({
     width: '100vw',
     height: '100vh',
@@ -28,13 +30,28 @@ function App() {
     getPins()
   }, [])
 
+  const handleMarkerClick = (id, lat, long) => {
+    setCurrentPlaceId(id)
+    setViewport({ ...viewport, latitude: lat, longitude: long })
+  }
+
+  const handleAddClick = (e) => {
+    const [longitude, latitude] = e.lngLat
+    setNewPlace({
+      lat: latitude,
+      long: longitude,
+    })
+  }
+
   return (
     <div className='App'>
       <ReactMapGL
         {...viewport}
         mapboxApiAccessToken={process.env.REACT_APP_MAPBOX}
         onViewportChange={(nextViewport) => setViewport(nextViewport)}
-        mapStyle='mapbox://styles/samkiroko/ckrhfb4wq6bdj17qyhlkkf1ja'>
+        mapStyle='mapbox://styles/samkiroko/ckrhfb4wq6bdj17qyhlkkf1ja'
+        onDblClick={handleAddClick}
+        transitionDuration='200'>
         {pins.map((p) => (
           <>
             <Marker
@@ -77,6 +94,53 @@ function App() {
             )}
           </>
         ))}
+        {newPlace && (
+          <>
+            <Marker
+              latitude={newPlace.lat}
+              longitude={newPlace.long}
+              offsetLeft={-3.5 * viewport.zoom}
+              offsetTop={-7 * viewport.zoom}>
+              <Room
+                style={{
+                  fontSize: 7 * viewport.zoom,
+                  color: 'tomato',
+                  cursor: 'pointer',
+                }}
+              />
+            </Marker>
+            <Popup
+              latitude={newPlace.lat}
+              longitude={newPlace.long}
+              closeButton={true}
+              closeOnClick={false}
+              onClose={() => setNewPlace(null)}
+              anchor='left'>
+              {/* <div>
+                <form onSubmit={handleSubmit}>
+                  <label>Title</label>
+                  <input placeholder='Enter a title' autoFocus onChange={(e) => setTitle(e.target.value)} />
+                  <label>Description</label>
+                  <textarea
+                    placeholder='Say us something about this place.'
+                    onChange={(e) => setDesc(e.target.value)}
+                  />
+                  <label>Rating</label>
+                  <select onChange={(e) => setStar(e.target.value)}>
+                    <option value='1'>1</option>
+                    <option value='2'>2</option>
+                    <option value='3'>3</option>
+                    <option value='4'>4</option>
+                    <option value='5'>5</option>
+                  </select>
+                  <button type='submit' className='submitButton'>
+                    Add Pin
+                  </button>
+                </form>
+              </div> */}
+            </Popup>
+          </>
+        )}
       </ReactMapGL>
     </div>
   )
