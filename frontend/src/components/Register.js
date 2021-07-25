0e65c5580f8190dadf2dd1994e2df7262b023cce
Samkiroko <kirokodev@gmail.com>
2021-07-25 13:25:17 +0300

    posting user to Db

--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -1,22 +1,48 @@
-import { Room } from '@material-ui/icons'
-import React from 'react'
+import { Cancel, Room } from '@material-ui/icons'
+import axios from 'axios'
+import React, { useRef, useState } from 'react'
 import './register.css'
 
 export default function Register() {
+  const [success, setSuccess] = useState(false)
+  const [error, setError] = useState(false)
+  const [registering, setRegistering] = useState(false)
+  const [showLogin, setShowLogin] = useState(false)
+  const nameRef = useRef()
+  const emailRef = useRef()
+  const passwordRef = useRef()
+
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+    const newUser = {
+      username: nameRef.current.value,
+      email: emailRef.current.value,
+      password: passwordRef.current.value,
+    }
+    try {
+      await axios.post('/users/register', newUser)
+      setError(false)
+      setSuccess(true)
+    } catch (err) {
+      setError(true)
+    }
+  }
+
   return (
     <div className='registerContainer'>
       <div className='logo'>
         <Room />
         SamPin
       </div>
-      <form>
-        <input type='text' placeholder='username' />
-        <input type='email' placeholder='email' />
-        <input type='text' placeholder='register' />
+      <form onSubmit={handleSubmit}>
+        <input type='text' placeholder='username' ref={nameRef} />
+        <input type='email' placeholder='email' ref={emailRef} />
+        <input type='password' placeholder='password' ref={passwordRef} />
         <button className='registerBtn'> Register </button>
-        <span className='success'>Successful.You can login now!</span>
-        <span className='failure'>Something went wrong!</span>
+        {success && <span className='success'>Successful.You can login now!</span>}
+        {error && <span className='failure'>Something went wrong!</span>}
       </form>
+      <Cancel className='registerCancel' />
     </div>
   )
 }
